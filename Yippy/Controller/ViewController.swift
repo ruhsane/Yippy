5854d4a932282e147edaec23908c8d50e213013d
Rushani Shawuti <ruhsane924@gmail.com>
2018-12-12 03:00:39 -0800

    [UI] constrainst and view changed for login page

--- Yippy/Controller/ViewController.swift
+++ Yippy/Controller/ViewController.swift
@@ -18,8 +18,11 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
         return signInButton
     }()
     
+    let background = UIImageView(image: #imageLiteral(resourceName: "Yippy_Splash.png"))
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(background)
         
         view.addSubview(signInButton)
         
@@ -27,7 +30,8 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
 //        GIDSignIn.sharedInstance().delegate = self
 //        GIDSignIn.sharedInstance().signIn()
         GIDSignIn.sharedInstance().signInSilently()
-
+        
+        constraints()
 
     }
 
@@ -35,3 +39,21 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
 
 }
 
+
+extension ViewController {
+    
+    func constraints() {
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        signInButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -100).isActive = true
+        signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+
+        
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        background.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        background.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        background.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    }
+    
+}
